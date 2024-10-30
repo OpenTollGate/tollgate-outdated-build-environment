e56b5b8e114dd5a91ccf59dbebb9cc272a9aaeee
Your Name <you@example.com>
2024-10-30 22:28:52 +0100

    Upload binauth etc..

--- FAService/var/www/upload.sh
+++ FAService/var/www/upload.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 VPS_IP="78.47.249.97"
+ROUTER_IP="192.168.8.1"
 
-# Upload files
+# Upload files to FAS server (VPS)
 scp *.php ../../../files/cgi-bin/curl_request.sh root@${VPS_IP}:/var/www/fas-hid/.
 
 # Run permission commands on remote server
@@ -11,4 +12,16 @@ sudo apt install -y jq
 touch /tmp/arguments_log.md
 chown www-data:www-data /tmp/arguments_log.md
 chmod 777 /tmp/arguments_log.md
+"
+
+# Upload files to OpenWRT router
+scp ../../../files/etc/config/opennds root@${ROUTER_IP}:/etc/config/opennds
+scp ../../../files/usr/lib/opennds/custombinauth.sh root@${ROUTER_IP}:/usr/lib/opennds/custombinauth.sh
+scp ../../../files/usr/lib/opennds/binauth_log.sh root@${ROUTER_IP}:/usr/lib/opennds/binauth_log.sh
+
+# Set permissions on router
+ssh root@${ROUTER_IP} "
+chmod +x /usr/lib/opennds/custombinauth.sh
+chmod +x /usr/lib/opennds/binauth_log.sh
+/etc/init.d/opennds restart
 "
