949e022276dfff69f8d2944fa926b8a026cb30a6
Your Name <you@example.com>
2024-10-31 14:02:13 +0100

    Currently most things are pingable, but not the FAService
    97.249.47.78

--- files/root/configure_fas.sh
+++ files/root/configure_fas.sh
@@ -5,27 +5,26 @@ FAS_IP="78.47.249.97"
 FAS_PORT="80"
 ROUTER_IP="192.168.8.1"
 
-# In configure_fas.sh
-# OpenNDS Walled Garden - expanded version
-uci add_list opennds.@opennds[0].walledgarden="$FAS_IP"
-uci add_list opennds.@opennds[0].walledgarden="$FAS_IP:$FAS_PORT"
-uci add_list opennds.@opennds[0].walledgarden="$FAS_IP/fas-hid_cashu.php"
-uci add_list opennds.@opennds[0].walledgarden="static.97.249.47.78.clients.your-server.de"
-uci add_list opennds.@opennds[0].walledgarden="allow_all_x_requested_with"
+# Set up WAN DNS first
+uci set network.wan.dns='8.8.8.8 8.8.4.4'  # Google DNS servers
+uci set network.wan.peerdns='0'            # Don't accept DNS from DHCP
 
-# Add DNS entries for status client and FAS
+# Configure DNSMasq to use Google DNS as upstream
+uci del_list dhcp.@dnsmasq[0].server='8.8.8.8' # Remove if exists
+uci add_list dhcp.@dnsmasq[0].server='8.8.8.8' # Add as main upstream DNS
+
+# Special DNS entries for FAS and status client
 uci add_list dhcp.@dnsmasq[0].address="/status.client/$ROUTER_IP"
 uci add_list dhcp.@dnsmasq[0].address="/status.client/login/$ROUTER_IP"
-uci add_list dhcp.@dnsmasq[0].server="$FAS_IP"
-uci add_list dhcp.@dnsmasq[0].address="/fas-hid_cashu.php/$FAS_IP"
-uci add_list dhcp.@dnsmasq[0].address="/$FAS_IP/$FAS_IP"
-uci add_list dhcp.@dnsmasq[0].address="/fas.service/$FAS_IP"
-
-# Add upstream DNS server for FAS
 uci add_list dhcp.@dnsmasq[0].server="/clients.your-server.de/8.8.8.8"
 uci add_list dhcp.@dnsmasq[0].server="/static.97.249.47.78.clients.your-server.de/$FAS_IP"
 
-# Firewall Rules - Allow DNS and HTTP
+# OpenNDS Walled Garden
+uci add_list opennds.@opennds[0].walledgarden="$FAS_IP"
+uci add_list opennds.@opennds[0].walledgarden="$FAS_IP:$FAS_PORT"
+uci add_list opennds.@opennds[0].walledgarden="$FAS_IP/fas-hid_cashu.php"
+
+# Firewall Rules
 uci add firewall rule
 uci set firewall.@rule[-1].name='Allow-FAS-HTTP'
 uci set firewall.@rule[-1].src='lan'
@@ -34,44 +33,20 @@ uci set firewall.@rule[-1].dest_ip="$FAS_IP"
 uci set firewall.@rule[-1].dest_port="$FAS_PORT"
 uci set firewall.@rule[-1].target='ACCEPT'
 
-# Allow DNS resolution
+# Allow DNS queries
 uci add firewall rule
 uci set firewall.@rule[-1].name='Allow-DNS'
 uci set firewall.@rule[-1].src='lan'
-uci  set firewall.@rule[-1].proto='tcp udp'
+uci set firewall.@rule[-1].proto='tcp udp'
 uci set firewall.@rule[-1].dest_port='53'
 uci set firewall.@rule[-1].target='ACCEPT'
-
 
 # Network Route
 uci add network route
 uci set network.@route[-1].target="$FAS_IP"
 uci set network.@route[-1].gateway="$ROUTER_IP"
-uci set network.@route[-1].interface='lan'
-
-# Add masquerading for FAS
-uci add firewall rule
-uci set firewall.@rule[-1].name='Masquerade-FAS'
-uci set firewall.@rule[-1].src='lan'
-uci set firewall.@rule[-1].dest='*'
-uci set firewall.@rule[-1].masq='1'
-
-# Add NAT rule for FAS
-uci add firewall redirect
-uci set firewall.@redirect[-1].name='NAT-FAS-HTTP'
-uci set firewall.@redirect[-1].src='lan'
-uci set firewall.@redirect[-1].proto='tcp'
-uci set firewall.@redirect[-1].src_dport='80'
-uci set firewall.@redirect[-1].dest_ip="$FAS_IP"
-uci set firewall.@redirect[-1].dest_port="$FAS_PORT"
-uci set firewall.@redirect[-1].target='DNAT'
-
-# Add static route
-uci add network route
-uci set network.@route[-1].target="$FAS_IP"
-uci set network.@route[-1].gateway="$ROUTER_IP"
 uci set network.@route[-1].interface='wan'
-
+uci set network.@route[-1].metric='1'
 
 # Apply all changes
 uci commit opennds
