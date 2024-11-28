436814f9b785f1c187c407b88baf3eb3158c56e2
Your Name <you@example.com>
2024-11-28 12:51:15 +0100

    Start scanner

--- files/nostr/shell/process_auth.sh
+++ files/nostr/shell/process_auth.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
+relay=$1
+purser=$2
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Please provide relay and purser as arguemnts"
+    exit 1
+fi
+
+
+echo "Relay: $relay"
+echo "Purser: $purser"
+# wss://tollbooth.stens.dev a057f743eca9efe9c97e6358bf2b37b05349029edfaa1a5e6e0c117fc95b9114
+
 # Get the latest authorization event
-auth_event=$(./get_latest_authorize.sh wss://tollbooth.stens.dev a057f743eca9efe9c97e6358bf2b37b05349029edfaa1a5e6e0c117fc95b9114)
+auth_event=$(./get_latest_authorize.sh $relay $purser)
 
 # Check if auth_event is empty
 if [ -z "$auth_event" ]; then
