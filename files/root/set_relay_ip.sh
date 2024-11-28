0215553eb12fc1b1c2538d5b05d0581ec4fbb1b5
Your Name <you@example.com>
2024-11-28 23:08:44 +0100

    Set relay_ip

--- files/root/set_relay_ip.sh
+++ files/root/set_relay_ip.sh
@@ -7,4 +7,11 @@ SCAN_OUTPUT=$(./scan_ports.sh 119.201.26 7777)
 FIRST_IP=$(echo "$SCAN_OUTPUT" | grep -A1 "Devices responding on port" | tail -n1)
 
 # Print the first IP
-echo "$FIRST_IP"
+echo "Relay IP: $FIRST_IP"
+
+# Update lighttpd.conf with the new IP
+# Using sed to replace the IP address in the proxy.server configuration
+sed -i "s/\"host\" => \"[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\"/\"host\" => \"$FIRST_IP\"/" /etc/lighttpd/lighttpd.conf
+
+# Restart lighttpd to apply changes
+/etc/init.d/lighttpd restart
